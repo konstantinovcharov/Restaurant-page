325b23b8b5eadea8f1fb8235a04dfad1ec90c0d8
Konstantin Ovcharov <konstantinovcharov@abv.bg>
2023-03-07 18:30:05 +0200

    main is now properly filled with content

--- src/index.js
+++ src/index.js
@@ -1,22 +1,75 @@
-import { loadHeader, loadMain, loadFooter } from "./loadPage.js";
+import { loadHeader, loadMain, fillMain, loadFooter } from "./loadPage.js";
 import { loadMenu } from "./loadMenu.js";
 import { loadContactPage } from "./loadContact.js";
 
 const contentDiv = document.getElementById('content');
+// const mainDiv = document.createElement('main');
+contentDiv.appendChild(loadHeader());
+contentDiv.appendChild(loadMain());
+const mainDiv = document.querySelector('main');
+const { sectionHero, sectionFeaturedItems } = fillMain();
+mainDiv.appendChild(sectionHero);
+mainDiv.appendChild(sectionFeaturedItems);
+// contentDiv.appendChild(mainDiv);
+contentDiv.appendChild(loadFooter());
+
+
+
+
+
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    contentDiv.appendChild(loadHeader());
-    contentDiv.appendChild(loadMain());
-    contentDiv.appendChild(loadFooter());
+    // mainDiv.appendChild(fillMain());
+    // const footer = document.querySelector('footer');
+    const homeButton = document.getElementById('Home');
+    const menuButton = document.getElementById('Menu');
+    const contactButton = document.getElementById('Contact');
+    // const menuTab = document.querySelector('.menu-tab');
 
-    // const navigationBar = document.querySelector('nav');
+    homeButton.addEventListener('click', loadMain2);
+    function loadMain2() {
+        mainDiv.innerHTML = ``;
+        mainDiv.appendChild(sectionHero);
+        mainDiv.appendChild(sectionFeaturedItems);
+        // mainDiv.appendChild(fillMain());
+        // mainDiv.style.display = "none";
+        // footer.style.display = "none";
+        //mainDiv.innerHTML = ``;
+        // footer.style.display = "none";
+        // menuTab.innerHTML = ``;
+        // contentDiv.appendChild(loadMain());
+        // contentDiv.appendChild(loadFooter());
+    }
+
+
+
+    menuButton.addEventListener('click', loadMenu2);
+    function loadMenu2() {
+        mainDiv.innerHTML = ``;
+        mainDiv.appendChild(loadMenu());
+        // footer.style.display = "block";
+        // menuTab.innerHTML = ``;
+        // footer.insertAdjacentElement("beforebegin", loadMenu());
+        // contentDiv.appendChild(loadMenu());
+    }
+
+    contactButton.addEventListener('click', loadContact2);
+    function loadContact2() {
+        mainDiv.innerHTML = ``;
+        mainDiv.appendChild(loadContactPage());
+        // footer.style.display = "block";
+
+    }
+
+
     // navigationBar.addEventListener('click', (e) => {
     //     if (e.target.id = "Menu") {
-    //         mainDiv.innerHTML = "";
+    //         console.log(e);
+    //         mainDiv.TextContent = "";
     //         mainDiv.appendChild(loadMenu());
     //     } else if (e.target.id = "Contact") {
-    //         mainDiv.innerHTML = "";
+    //         mainDiv.TextContent = "";
     //         mainDiv.appendChild(loadContactPage());
     //     } else if (e.target.id = "Home") {
     //         return;
--- src/loadPage.js
+++ src/loadPage.js
@@ -8,15 +8,12 @@ export function loadHeader() {
     const navLi2 = document.createElement('li');
     const navLi3 = document.createElement('li');
     const navA1 = document.createElement('a');
-    navA1.setAttribute('href', '#');
     navA1.setAttribute('id', 'Home');
     navA1.textContent = "Home";
     const navA2 = document.createElement('a');
-    navA2.setAttribute('href', '#');
     navA2.setAttribute('id', "Menu");
     navA2.textContent = "Menu";
     const navA3 = document.createElement('a');
-    navA3.setAttribute('href', '#');
     navA3.textContent = "Contact";
     navA3.setAttribute('id', 'Contact');
     navLi1.appendChild(navA1);
@@ -32,31 +29,41 @@ export function loadHeader() {
     return header;
 }
 
-export function loadMain() {
+export function loadMain() {   
+    const main = document.createElement('main'); 
+    return main;
+}
+
+export function fillMain() {
     // Section hero html
-    const main = document.createElement('main');
     const sectionHero = document.createElement('section');
     sectionHero.classList.add('hero');
+
     const sectionHeroH2 = document.createElement('h2');
     sectionHeroH2.textContent = "Welcome to our restaurant";
+
     const sectionHeroP = document.createElement('p');
     sectionHeroP.textContent = "Enjoy our delicious food and great atmosphere.";
+
     const sectionHeroA = document.createElement('a');
     sectionHeroA.setAttribute('href', '#');
     sectionHeroA.classList.add('cta');
     sectionHeroA.textContent = "See our menu";
+
     sectionHero.appendChild(sectionHeroH2);
     sectionHero.appendChild(sectionHeroP);
-    sectionHero.appendChild(sectionHeroA);
-    main.appendChild(sectionHero);
+    sectionHero.appendChild(sectionHeroA);    
 
     // Section featured items html
     const sectionFeaturedItems = document.createElement('section');
     sectionFeaturedItems.classList.add('featured-items');
+
     const sectionFeaturedItemsH2 = document.createElement('h2');
     sectionFeaturedItemsH2.textContent = "Featured Items";
+
     const sectionFeaturedItemsDiv1 = document.createElement('div');
     sectionFeaturedItemsDiv1.classList.add('item');
+
     const Div1Img = document.createElement('img');
     Div1Img.setAttribute('src', '../src/food1.jpg');
     Div1Img.setAttribute('alt', 'Item 1');
@@ -68,6 +75,7 @@ export function loadMain() {
     Div1A.setAttribute('href', '#');
     Div1A.classList.add('cta');
     Div1A.textContent = "Order now";
+
     sectionFeaturedItemsDiv1.appendChild(Div1Img);
     sectionFeaturedItemsDiv1.appendChild(Div1H3);
     sectionFeaturedItemsDiv1.appendChild(Div1P);
@@ -75,9 +83,10 @@ export function loadMain() {
 
     const sectionFeaturedItemsDiv2 = document.createElement('div');
     sectionFeaturedItemsDiv2.classList.add('item');
+
     const Div2Img = document.createElement('img');
     Div2Img.setAttribute('src', '../src/food2.jpeg');
-    Div2Img.setAttribute('alt', 'Item 2');
+    Div2Img.setAttribute('alt', 'Item 2');    
     const Div2H3 = document.createElement('h3');
     Div2H3.textContent = "Item 2";
     const Div2P = document.createElement('p');
@@ -86,6 +95,7 @@ export function loadMain() {
     Div2A.setAttribute('href', '#');
     Div2A.classList.add('cta');
     Div2A.textContent = "Order now";
+
     sectionFeaturedItemsDiv2.appendChild(Div2Img);
     sectionFeaturedItemsDiv2.appendChild(Div2H3);
     sectionFeaturedItemsDiv2.appendChild(Div2P);
@@ -93,6 +103,7 @@ export function loadMain() {
 
     const sectionFeaturedItemsDiv3 = document.createElement('div');
     sectionFeaturedItemsDiv3.classList.add('item');
+
     const Div3Img = document.createElement('img');
     Div3Img.setAttribute('src', '../src/food3.jpeg');
     Div3Img.setAttribute('alt', 'Item 3');
@@ -104,6 +115,7 @@ export function loadMain() {
     Div3A.setAttribute('href', '#');
     Div3A.classList.add('cta');
     Div3A.textContent = "Order now";
+
     sectionFeaturedItemsDiv3.appendChild(Div3Img);
     sectionFeaturedItemsDiv3.appendChild(Div3H3);
     sectionFeaturedItemsDiv3.appendChild(Div3P);
@@ -112,11 +124,9 @@ export function loadMain() {
     sectionFeaturedItems.appendChild(sectionFeaturedItemsH2);
     sectionFeaturedItems.appendChild(sectionFeaturedItemsDiv1);
     sectionFeaturedItems.appendChild(sectionFeaturedItemsDiv2);
-    sectionFeaturedItems.appendChild(sectionFeaturedItemsDiv3);
+    sectionFeaturedItems.appendChild(sectionFeaturedItemsDiv3);    
 
-    main.appendChild(sectionFeaturedItems);
-
-    return main;
+    return {sectionHero, sectionFeaturedItems};
 }
 
 export function loadFooter() {
